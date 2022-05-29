8b84d2926a857bee811bf10a5ed11a4cdfd6627f
Tymofii Smyrnov <tymofii.smyrnov@gmail.com>
2022-05-29 22:24:49 +0300

    CRUD practice

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (View,
+                                  TemplateView,
+                                  ListView,
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView, DeleteView)
 from . import models
 
 
@@ -16,3 +22,18 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    model = models.School
+    fields = ['name', 'principal', 'location']
+
+
+class SchoolUpdateView(UpdateView):
+    model = models.School
+    fields = ['name', 'principal']
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
